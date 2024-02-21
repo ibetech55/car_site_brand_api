9e65f18d474872e7636cca6d4ff01ba186579bd5
pibewiro <pibewiro@yahoo.com>
2024-02-20 23:55:15 -0300

    chore: fix cors and default headers

--- src/Infra/HttpServer/index.ts
+++ src/Infra/HttpServer/index.ts
@@ -83,7 +83,7 @@ class HttpServer {
         "Content-Type, Authorization"
       );
 
-      res.setHeader("Access-Control-Allow-Origin", origin);
+      res.setHeader("Access-Control-Allow-Origin", CAR_SITE_FRONTEND_URL);
       res.setHeader(
         "Access-Control-Allow-Methods",
         "GET, POST, PUT, DELETE, PATCH"
